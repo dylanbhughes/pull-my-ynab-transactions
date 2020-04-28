42b0b1105e35f04e73c97ea295b1e3f248d86b5e
dylanbhughes <dylanbhughes@gmail.com>
2020-04-28 16:44:30 -0400

    Basics working

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # pull-my-ynab-transactions
+
+Prefect flow to import transactions into YNAB from your linked bank accounts without having to remember to sign in.
---
+++ flow.py
@@ -0,0 +1,23 @@
+from prefect import task, Flow
+from prefect.tasks.secrets import Secret
+import requests
+from datetime import timedelta
+
+
+@task(
+    name="Ping YNAB to Import Transactions",
+    max_retries=10,
+    retry_delay=timedelta(seconds=30),
+)
+def ping_ynab_to_import_transactions(api_key):
+    response = requests.post(
+        url="https://api.youneedabudget.com/v1/budgets/last-used/transactions/import",
+        headers={"Authorization": f"Bearer {api_key}"},
+    )
+    print(response)
+    return response
+
+
+with Flow(name="Ping YNAB") as flow:
+    API_KEY = Secret("YNAB_API_KEY")
+    result = ping_ynab_to_import_transactions(api_key=API_KEY)
